f27bbac4e11ce72a5db397344790a490ec4ca136
Saket Upperla <saketupperla7@gmail.com>
2024-08-18 19:40:24 -0500

    updated responsive

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -5,7 +5,7 @@ const WorkExperience = () => {
   return (
     <section id='work-experience' className='flex flex-col items-center justify-center w-screen mb-20'>
       <div className='flex flex-col items-center justify-start w-full py-8'>
-        <h1 className="font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100.8px] leading-[75px]">
+        <h1 className="font-poppins font-semibold text-4xl sm:text-5xl lg:text-6xl leading-tight">
           <span className="text-gradient">Work Experience</span>
         </h1>
       </div>
@@ -49,17 +49,17 @@ const Pillar = () => {
 const EventCard = ({ heading, date }) => {
   return (
     <div className='transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl flex flex-col gap-y-2 justify-center border shadow-md rounded-xl p-4 w-48 sm:w-60 lg:w-90 justify-self-end overflow-hidden'>
-      <div className='font-bold text-lg text-yellow-400'>{heading}</div>
-      <div className='text-sm text-white'>{date}</div>
+      <div className='font-bold text-base sm:text-lg md:text-xl text-yellow-400'>{heading}</div>
+      <div className='text-xs sm:text-sm md:text-base text-white'>{date}</div>
     </div>
   );
 }
 
 const Description = ({ subHeading, description }) => {
   return (
-    <div className='transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl flex flex-col gap-y-2 justify-center border shadow-md rounded-xl p-4 w-120 sm:w-96 xs:w-full justify-self-endmax-w-lg overflow-hidden'>
-      <div className='font-bold text-lg text-orange-400'>{subHeading}</div>
-      <div className='text-sm text-white'>{description}</div>
+    <div className='transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-2xl flex flex-col gap-y-2 justify-center border shadow-md rounded-xl p-4 w-120 sm:w-96 xs:w-full justify-self-end max-w-lg overflow-hidden'>
+      <div className='font-bold text-sm sm:text-lg md:text-xl text-orange-400'>{subHeading}</div>
+      <div className='text-xs sm:text-sm md:text-base text-white'>{description}</div>
     </div>
   )
 }
